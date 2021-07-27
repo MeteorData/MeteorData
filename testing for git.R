a3d0f5f7c2829bca23019bdae160463bbb85f1b2
MeteorData <87923288+MeteorData@users.noreply.github.com>
2021-07-27 13:41:07 +0530

    Add files via upload

---
+++ testing for git.R
@@ -0,0 +1,17 @@
+print("testing for file created in rstudio 27")
+
+print("for github 27")
+
+print("git")
+
+activatedata()
+lasttab()
+help()
+example(help)
+binom.test()
+example(histogram)
+help(mean)
+close()
+exit()
+git config --global color.ui
+print("got frustrated a little")
